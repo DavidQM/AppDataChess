3c5a68403f91032716c310f0cfb1e21b217b14cc
caosaje1 <caosaje1@gmail.com>
2017-06-08 00:53:19 -0500

    Login google & facebook 3.0

--- app/src/main/java/com/example/viper2/appdatachess/LoginActivity.java
+++ app/src/main/java/com/example/viper2/appdatachess/LoginActivity.java
@@ -82,7 +82,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
